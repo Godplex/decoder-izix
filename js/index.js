7fd40c382c0922dcb15c5a6eb2259e68541a865d
Julian Rincon <julian1918@hotmail.es>
2022-01-20 12:46:40 -0500

    clean text area

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
 var form = document.querySelector('form');
 var encrypt = document.querySelector('#encrypt');
 var decrypt = document.querySelector('#decrypt');
-
-function captureText(form) {
-    var addText = form.addText.value;
-    return addText;
-}
+var deleteText = document.querySelector('#deleteText');
+var showText = document.querySelector("#showText");
+var noText = document.querySelector("#no-text");
+var withText = document.querySelector("#with-text");
+var addText = document.querySelector("#addText");
 
 function validText(fun) {
-    var str = captureText(form);
-    var showText = document.querySelector("#showText");
-    var noText = document.querySelector("#no-text");
-    var withText = document.querySelector("#with-text");
+    var str = addText.value;
     if (str.length > 0) {
         noText.classList.add("d-none");
         withText.classList.remove("d-none");
@@ -21,5 +18,28 @@ function validText(fun) {
         withText.classList.add("d-none");
     }
 }
+
+deleteText.addEventListener("click", function () {
+    if (addText.value.length > 0) {
+        addText.value = '';
+        noText.classList.remove("d-none");
+        withText.classList.add("d-none");
+        addText.focus();
+    }
+});
+
+deleteText.addEventListener("click", hideButton);
+['paste', 'keyup'].forEach(e => addText.addEventListener(e, hideButton));
+
+function hideButton() {
+    if (addText.value.length > 0) {
+        deleteText.classList.add("d-none");
+        deleteText.classList.remove("d-none");
+    } else {
+        deleteText.classList.remove("d-none");
+        deleteText.classList.add("d-none");
+    }
+}
+
 
 
